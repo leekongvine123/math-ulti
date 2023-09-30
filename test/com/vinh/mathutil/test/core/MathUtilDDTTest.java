ff164eb0f389406e442a30910d9313870d4e8f68
leekongvine123 <congvinhdt1223@gmail.com>
2023-09-30 21:08:19 +0700

    5-120

--- test/com/vinh/mathutil/test/core/MathUtilDDTTest.java
+++ test/com/vinh/mathutil/test/core/MathUtilDDTTest.java
@@ -35,7 +35,7 @@ public class MathUtilDDTTest {
                 {2,2},
                 {3,6},
                 {4,24},
-                {7,120},
+                {5,120},
             };
             
         }
