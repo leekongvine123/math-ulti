0df725f72a4586a14cf7a6fff74acf22bdd96720
leekongvine123 <congvinhdt1223@gmail.com>
2023-10-03 00:30:44 +0700

    refactor the main code and greeen 4ever

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 ### You will find in this repo the following stuff:
 * The Math-Util library that offers multiple math function(a Java-based source code
 * Unit Testing source code with JUnit framework
-* ...
+* GitHub Actions to demonstrate the Continuos Integration
+* Regression Test
 #### © 2023 leekongvine
 
 ### Sreenshots
--- src/com/vinh/mathutil/core/MathUtil.java
+++ src/com/vinh/mathutil/core/MathUtil.java
@@ -21,23 +21,48 @@ public class MathUtil {
     // gia thừa hàm đồ thị dốc đứng, tăng nhanh về giá trị
     // 20 giai thừa 18 con số 0, vừa ịp đủ cho kiểu long của Java
     //bài này quy ước tính n! trong khoảng từ 0...20
-    public static long getFactorial(int n){
-        
+//    public static long getFactorial(int n){
+//        
+//        if(n<0||n>20){
+//            throw new IllegalArgumentException("Invalid argument. N must be between 0....20");
+//            
+//        }
+//        if(n==0 || n==1){
+//            return 1;// kết thức cuộc chơi sớm nếu nhận được những dầu vào đặc biệt
+//        }
+//        long product =1;//tích nhân dồn, thuật toán heo đất, ốc bu dồn thịt
+//        
+//        for (int i = 2; i <=n; i++){
+//            product *=i;
+//            
+//        }
+//        return product;
+//    }
+    //Hoc de quy trong vong 30s -RECURSION
+    //Hien tuong goi lai chinh minh voi 1 quy mo khac
+    //bup be to, nho hon, nho hon nua, nho hon nua...den diem dung
+    
+    //tinh gium tui 6!
+    //n! = 1.2.3.4.5.6...n
+    //6! = 6x 5!
+    //5!= 5x4!
+    //4!=4x 3!
+    //3! = 3 x 2!
+    //2! = 2 x 1!
+    //1! =1// diem dung
+    
+    //quy uoc 1! = 0! = 1;
+    // chot ha: n! - n*(n -1)!
+    
+        public static long getFactorial(int n){
         if(n<0||n>20){
-            throw new IllegalArgumentException("Invalid argument. N must be between 0....20");
-            
+            throw new IllegalArgumentException("n must be bewteen 0..20 plss");
         }
-        if(n==0 || n==1){
-            return 1;// kết thức cuộc chơi sớm nếu nhận được những dầu vào đặc biệt
-        }
-        long product =1;//tích nhân dồn, thuật toán heo đất, ốc bu dồn thịt
-        
-        for (int i = 2; i <=n; i++){
-            product *=i;
-            
-        }
-        return product;
+      if(n==0 || n==1){
+       return 1;//ket thuc cuoc choi som neu nhan nhung dau vao dac biet
+      }
+        return n* getFactorial(n-1);
+      }
     }
     
-    
-}
+
